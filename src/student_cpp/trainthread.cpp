6c32af99fa8a69b8d81f97d8a5e0c645430e8ae6
marom17 <romain.flac@gmail.com>
2015-03-25 14:10:05 +0100

    Ajout des semaphores

--- src/student_cpp/trainthread.cpp
+++ src/student_cpp/trainthread.cpp
@@ -7,7 +7,11 @@ void TrainThread::run() {
     while(true) {
 
         for (int i = 1; i < parcour.size(); i++) {
+            train->arreter();
+            sections.at(parcour.at(i)-1)->acquire();
+            train->demarrer();
             attendre_contact(parcour.at(i));
+            sections.at(parcour.at(i)-1)->release();
             afficher_message(qPrintable(QString("The engine no. %1 has reached contact no. %2.").arg(train->numero()).arg(parcour.at(i))));
             train->afficherMessage(QString("I've reached contact no. %1.").arg(parcour.at(i)));
 
